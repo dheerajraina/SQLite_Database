49038a9d57ea739c55026d0494c2697ab7bb1808
Dheeraj Raina <dheerajraina55@gmail.com>
2024-08-28 13:13:59 +0530

    main file for 'create database command'

---
+++ src/main.c
@@ -0,0 +1,28 @@
+#include <stdio.h>
+#include <string.h>
+#include "sql_command.h"
+#include "parser.h"
+#include "execute.h"
+
+int main()
+{
+    char input[512];
+    SQLCommand command;
+
+    printf("Enter SQL command:\n");
+    fgets(input, sizeof(input), stdin);
+
+    // // Remove newline character from input
+    input[strcspn(input, "\n")] = 0;
+
+    if (parse_command(input, &command))
+    {
+        execute_command(&command);
+    }
+    else
+    {
+        printf("Invalid command.\n");
+    }
+
+    return 0;
+}
