f43f65426ff6e379707cf4dffd9ba7f812ba6766
Dheeraj Raina <dheerajraina55@gmail.com>
2024-08-30 13:19:02 +0530

    Additions for 'Create Table command

--- include/sql_command.h
+++ include/sql_command.h
@@ -1,13 +1,19 @@
 #ifndef SQL_COMMAND_H
 #define SQL_COMMAND_H
 
-typedef enum {
+#include "constants.h"
+
+typedef enum
+{
     CREATE_DATABASE,
+    CREATE_TABLE,
 } CommandType;
 
-typedef struct {
+typedef struct
+{
     CommandType type;
     char database_name[255]; // Assuming max length of the database name is 255 characters
+    TableSchema *schema;
 } SQLCommand;
 
 #endif
