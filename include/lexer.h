8ed51bba2e1e1bd7cd4c51f011991eca8e2819b6
Dheeraj Raina <dheerajraina55@gmail.com>
2024-08-30 13:18:31 +0530

    Additions for 'Create Table command

--- include/lexer.h
+++ include/lexer.h
@@ -1,13 +1,37 @@
 #ifndef LEXER_H
 #define LEXER_H
 
-#define MAX_TOKENS 3
+#define MAX_TOKENS 100
 
-typedef struct {
-    char *tokens[MAX_TOKENS];
+typedef enum
+{
+    TOKEN_CREATE,      // 0
+    TOKEN_DATABASE,    // 1
+    TOKEN_TABLE,       // 2
+    TOKEN_IDENTIFIER,  // 3
+    TOKEN_DATA_TYPE,   // 4
+    TOKEN_NOT_NULL,    // 5
+    TOKEN_PRIMARY_KEY, // 6
+    TOKEN_LPAREN,      // 7
+    TOKEN_RPAREN,      // 8
+    TOKEN_COMMA,       // 9
+    TOKEN_SEMICOLON,   // 10
+    TOKEN_EOF,         // 11
+    TOKEN_UNKNOWN      // 12
+} TokenType;
+
+typedef struct
+{
+    TokenType type;
+    char *value;
+} Token;
+typedef struct
+{
+    Token *tokens[MAX_TOKENS];
     int token_count;
 } Lexer;
 
+Token *get_next_token(char *input);
 
 void init_lexer(Lexer *lexer, char *input);
 
--- src/lexer.c
+++ src/lexer.c
@@ -1,17 +1,88 @@
 #include <stdio.h>
 #include <string.h>
+#include <stdlib.h>
+#include <ctype.h>
 #include "lexer.h"
 #include "sql_command.h"
 
+Token *get_next_token(char *input)
+{
+    Token *token = malloc(sizeof(Token));
 
+    // printf("get_next_token ---------- %s", input);
+    if (isalpha(*input))
+    {
+        if (strcmp(input, "CREATE") == 0)
+        {
+            token->type = TOKEN_CREATE;
+        }
+        else if (strcmp(input, "DATABASE") == 0)
+        {
+            token->type = TOKEN_DATABASE;
+        }
+        else if (strcmp(input, "TABLE") == 0)
+        {
+            token->type = TOKEN_TABLE;
+        }
+        else if (strcmp(input, "INTEGER") == 0 || strcmp(input, "TEXT") == 0)
+        {
+            token->type = TOKEN_DATA_TYPE;
+        }
+        else if (strcmp(input, "NOT_NULL") == 0)
+        {
+            token->type = TOKEN_NOT_NULL;
+        }
+        else if (strcmp(input, "PRIMARY_KEY") == 0)
+        {
+            token->type = TOKEN_PRIMARY_KEY;
+        }
+        else
+        {
+            token->type = TOKEN_IDENTIFIER;
+        }
+        token->value = input;
+    }
+    else if (*input == '(')
+    {
+        token->type = TOKEN_LPAREN;
+    }
+    else if (*input == ')')
+    {
+        token->type = TOKEN_RPAREN;
+    }
+    else if (*input == ',')
+    {
+        token->type = TOKEN_COMMA;
+    }
+    else if (*input == ';')
+    {
+        token->type = TOKEN_SEMICOLON;
+    }
+    else if (*input == '\0')
+    {
+        token->type = TOKEN_EOF;
+    }
+    else
+    {
+        token->type = TOKEN_UNKNOWN;
+    }
+    return token;
+}
 
-
-void init_lexer(Lexer *lexer, char *input) {
+void init_lexer(Lexer *lexer, char *input)
+{
     lexer->token_count = 0;
-    printf("%s",input);
     char *token = strtok(input, " ");
-    while (token != NULL && lexer->token_count < MAX_TOKENS) {
-        lexer->tokens[lexer->token_count++] = token;
+    printf("initial token %s", token);
+    while (token != NULL && lexer->token_count < MAX_TOKENS)
+    {
+        printf("%s\n", token);
+        lexer->tokens[lexer->token_count++] = get_next_token(token);
         token = strtok(NULL, " ");
+    }
+
+    for (size_t i = 0; i < lexer->token_count; i++)
+    {
+        printf("init_lexer------------%d %s \n", lexer->tokens[i]->type, lexer->tokens[i]->value);
     }
 }
